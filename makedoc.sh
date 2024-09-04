7a87dc6852698beff72b0c251115dde069370a5b
gkholman <gkenholman+github@gmail.com>
2024-09-04 11:00:02 -0400

    Generate stylesheet documentation

---
+++ makedoc.sh
@@ -0,0 +1,12 @@
+if [ ! -f utilities/xslstyle/xslstyle-docbook.xsl ] || \
+   [ ! -f utilities/saxon655/saxon.jar ]
+then
+echo Expecting the XSLStyle documentation subsystem is installed in the
+echo utilities/xslstyle/ directory: https://github.com/CraneSoftwrights/xslstyle and
+echo expecting the Saxon 6.5.5 XSLT 1 processor is installed in the
+echo utilities/saxon655/ directory:
+echo https://sourceforge.net/projects/saxon/files/saxon6/6.5.5/
+exit 1
+fi
+java -jar utilities/saxon655/saxon.jar -a -o designSVG2burnFiles.html designSVG2burnFiles.xsl
+open designSVG2burnFiles.html
